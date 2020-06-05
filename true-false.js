ee5123c4db085bf345439099c0f1261784e5aee5
AlifAkash <alifakash.cse.hstu@gmail.com>
2020-06-05 21:49:38 +0600

    Advanced start

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//falsy:
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+//truthy
+//'0' [] {} 
+//......baki sob
+
+const num="";
+if(num){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
