e7215ce5c887c746eaaf2ece61ef7dbb077c0c02
AlifAkash <alifakash.cse.hstu@gmail.com>
2020-06-08 00:19:51 +0600

    Null vs undefined cleared

---
+++ undefine_vs_null.js
@@ -0,0 +1,19 @@
+let turkey;
+
+function add(num1,num2){
+    console.log(num1+num2);
+    const sum= num1+num2;
+    return 
+}
+
+function add(num1,num2){
+    console.log(num1,num2);
+}
+const result=add(87);
+console.log(result);
+
+let fun= undefined;
+console.log(fun);
+
+const man={name:"Abul", phoneNo:546778};
+console.log(man.gf);
