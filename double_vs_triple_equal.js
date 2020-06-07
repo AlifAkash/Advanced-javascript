62d6652524e97bf1e8431016fa5bac7953158a07
AlifAkash <alifakash.cse.hstu@gmail.com>
2020-06-08 01:21:27 +0600

    map_filter done

---
+++ double_vs_triple_equal.js
@@ -0,0 +1,17 @@
+const first=2;
+const second="2";
+if(first==second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+const first1=2;
+const second1="2";
+if(first1===second1){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
